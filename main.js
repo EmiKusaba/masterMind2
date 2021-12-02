be60a34dfaa0684a23b01963ed4add66102dff7f
EmiKusaba <eeemi615@gmail.com>
2021-12-02 15:38:46 -0600

    added func

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 //Spec 0
 const masterMind = () => {
   const solution = 'abcd';
+  if(guess === solution){
+    return console.log('You guessed it!')
+  }
+}
+const generateHint = (guess) => {
+  let solutionArray = []
+  let guessArray =[]
+  guessArray = guess.split(' ')
 }
