67689187483bf911caf6862bdad8bf4c85818614
EmiKusaba <eeemi615@gmail.com>
2021-12-03 15:00:47 -0600

    added mastergame and generateHint func

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
+let board = [];
+let solution = '';
+let letters = ['a', 'b', 'c', 'd'];
+
 //Spec 0
 const masterMind = () => {
-  const solution = 'abcd';
-  if(guess === solution){
-    return console.log('You guessed it!')
+    if (solution === guess) {
+      return "You guessed it!";
+    }
+    let hint = generateHint(guess);
+    board.push(guess + " " + hint);
+  //Spec 4 - End the game 
+    if (board.length >= 10) {
+      return "You ran out of turns!";
+    }
+  
+    return "Guess again!";
   }
-}
-const generateHint = (guess) => {
-  let solutionArray = []
-  let guessArray =[]
-  guessArray = guess.split(' ')
+
+//Spec 2.2 - Determine correct "letter-locations
+const generateHint = (guess) =>{
+  let guessArray = guess.split("");
+  let solutionArray = solution.split("");
+
+  let correctLetterLocations = 0;
+  for (let i = 0; i < solutionArray.length; i++) {
+    if (solutionArray[i] === guessArray[i]) {
+      correctLetterLocations++;
+      solutionArray[i] = null;
+    }
+  }
+  //Spec 2.3 - Determine correct "letters"
+
+  let correctLetters = 0;
+  for(let i = 0; i <solutionArray.length; i++) {
+    var guessValue = guessArray[i];
+    var targetIndex = solutionArray.indexOf(guessValue);
+    if(targetIndex > -1) {
+    correctLetters++;
+    solutionArray[i] = null;
+    }
+  }
 }
