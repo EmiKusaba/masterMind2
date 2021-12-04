cccf8939d0ad5bbbf8fe7014e9b54a4e6d7024aa
EmiKusaba <eeemi615@gmail.com>
2021-12-04 13:32:46 -0600

    added test

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
+'use strict';
+
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
 let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd'];
 
+
+function printBoard() {
+  for (let i = 0; i < board.length; i++) {
+    console.log(board[i]);
+  }
+}
+
+function generateSolution() {
+  for (let i = 0; i < 4; i++) {
+    const randomIndex = getRandomInt(0, letters.length);
+    solution += letters[randomIndex];
+  }
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 //Spec 0
-const masterMind = () => {
+const mastermind = (guess) => {
     if (solution === guess) {
       return "You guessed it!";
     }
@@ -37,7 +65,49 @@ const generateHint = (guess) =>{
     var targetIndex = solutionArray.indexOf(guessValue);
     if(targetIndex > -1) {
     correctLetters++;
-    solutionArray[i] = null;
+    solutionArray[targetIndex] = null
     }
   }
+
+  return `${correctLetters}-${correctLetterLocations}`
+}
+
+
+
+function getPrompt() {
+  rl.question('guess: ', (guess) => {
+    mastermind(guess);
+    printBoard();
+    getPrompt();
+  });
+}
+
+// Tests
+
+if (typeof describe === 'function') {
+  solution = 'abcd';
+  describe('#mastermind()', () => {
+    it('should register a guess and generate hints', () => {
+      mastermind('aabb');
+      assert.equal(board.length, 1);
+    });
+    it('should be able to detect a win', () => {
+      assert.equal(mastermind(solution), 'You guessed it!');
+    });
+  });
+
+  describe('#generateHint()', () => {
+    it('should generate hints', () => {
+      assert.equal(generateHint('abdc'), '2-2');
+    });
+    it('should generate hints if solution has duplicates', () => {
+      assert.equal(generateHint('aabb'), '1-1');
+    });
+
+  });
+
+} else {
+
+  generateSolution();
+  getPrompt();
 }
